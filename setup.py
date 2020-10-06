4ba7dfce4f6e0beb687369c5aaf17ba2293d7340
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2020-10-06 17:43:56 +0100

    Update requirements.txt & setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(name='tools',
-      version='0.0.1',
-      description='Trend Barometer Tools',
-      author='...',
-      author_email='...',
-      packages=['tools'],
-      install_requires=['norgatedata',
-                        'requests',
-			'pandas',
-                        'numpy',
-                        'matplotlib',
-                        'talib',
-                        'time',
-                        'functools',
-			'yahoofinancials'])
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'trendbarometer'
+AUTHOR = '...'
+AUTHOR_EMAIL = '...'
+URL = 'https://github.com/GBERESEARCH/trendbarometer'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Measure which markets are more strongly trending and visualize.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      	'numpy'
+	'requests'
+	'pandas'
+	'matplotlib'
+	'norgatedata'
+	'yahoofinancials'
+	'talib-binary'
+	'spyder'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
 
