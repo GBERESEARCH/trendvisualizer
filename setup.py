b753811cdc22dcb5e76846c385581d6540cc0416
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2020-10-26 14:53:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 import pathlib
+import runpy
 from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+version_meta = runpy.run_path("./version.py")
+VERSION = version_meta["__version__"]
 PACKAGE_NAME = 'trendbarometer'
-AUTHOR = '...'
-AUTHOR_EMAIL = '...'
+AUTHOR = 'GBERESEARCH'
+AUTHOR_EMAIL = 'gberesearch@gmail.com'
 URL = 'https://github.com/GBERESEARCH/trendbarometer'
 
 LICENSE = 'MIT'
-DESCRIPTION = 'Measure which markets are more strongly trending and visualize.'
+DESCRIPTION = 'Measure trend strength across various markets and visualize.'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
-INSTALL_REQUIRES = [
-      	'numpy'
-	'requests'
-	'pandas'
-	'matplotlib'
-	'norgatedata'
-	'yahoofinancials'
-	'talib-binary'
-	'spyder'
-]
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 setup(name=PACKAGE_NAME,
       version=VERSION,
@@ -34,7 +30,7 @@ setup(name=PACKAGE_NAME,
       license=LICENSE,
       author_email=AUTHOR_EMAIL,
       url=URL,
-      install_requires=INSTALL_REQUIRES,
+      install_requires=parse_requirements("requirements.txt"),
       packages=find_packages()
       )
 
